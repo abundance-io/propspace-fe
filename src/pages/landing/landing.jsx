9ec60fb7ed6064ab0d59cfe114140e859c513831
tanthehack <31630356+tanthehack@users.noreply.github.com>
2024-01-27 16:45:26 +0100

    add: what we do section

--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -1,4 +1,5 @@
 import heroImg from "../../assets/images/hero.svg";
+import houseImg from "../../assets/images/house.jpg";
 import downIcon from "../../assets/icons/down.svg";
 import { Button } from "../../components/global/button";
 import { TextHighlighter } from "../../components/global/textHighlighter";
@@ -37,7 +38,7 @@ export const Landing = () => {
         <img src={downIcon} />
       </a>
 
-      <section className="" id="about">
+      <section className="flex flex-col gap-10 p-20" id="about">
         <div className="flex flex-col items-center space-y-5">
           <span className="bg-mustard rounded-[12px] px-5 py-3 text-sm font-bold text-black">
             what we do
@@ -45,6 +46,34 @@ export const Landing = () => {
           <h2 className="text-center text-6xl font-medium">
             Revolutionizing Real Estate <br /> Investment for Africans
           </h2>
+        </div>
+        <div className="flex flex-col items-center gap-10 lg:mt-[100px] lg:flex-row">
+          <div
+            style={{
+              backgroundImage: `url(${houseImg})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              filter: "grayscale(100%)",
+            }}
+            className="h-[200px] w-full rounded-[40px] lg:h-[500px] lg:w-1/2"
+          ></div>
+          <p className="text-xl lg:w-1/2">
+            <span className="text-mustard text-6xl">*</span>
+            Discover the{" "}
+            <TextHighlighter>
+              future of real estate on Propspace
+            </TextHighlighter>
+            , where blockchain turns properties into tradable NFTs. Co-own
+            properties, share profits, and participate in transparent governance
+            through our ICP blockchain DAO.
+            <span className="flex flex-col gap-4 pt-[100px] lg:flex-row lg:items-center">
+              <p className="lg:w-3/5">
+                Join us in reshaping real estate investment for a more inclusive
+                and prosperous future.
+              </p>
+              <Button text="Get Started" />
+            </span>
+          </p>
         </div>
       </section>
     </>
