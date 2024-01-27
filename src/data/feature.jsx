84f85eb665d4036a0e1c2a47f0caf4a0be8b692a
tanthehack <31630356+tanthehack@users.noreply.github.com>
2024-01-27 17:59:41 +0100

    add: features section

---
+++ src/data/feature.jsx
@@ -0,0 +1,30 @@
+export const FeaturesData = [
+  {
+    id: 1,
+    title: "NFT-based Unitization",
+    primary: false,
+    description:
+      "Propspace transforms properties into divisible NFTs using the DIP-721 standard, enabling easy tradability and digital ownership representation.",
+  },
+  {
+    id: 2,
+    title: "Streamlined Co-Ownership & Profit Sharing",
+    primary: true,
+    description:
+      "Experience collective ownership, seamlessly purchase units, and receive a share of property profits based on performance, ensuring a consistent and rewarding income stream.",
+  },
+  {
+    id: 3,
+    title: "Transparent DAO Governance",
+    primary: true,
+    description:
+      "Participate in decision-making through Propspace's DAO on the ICP blockchain, ensuring transparent governance and active co-owner involvement.",
+  },
+  {
+    id: 4,
+    title: "Decentralized Community Ecosystem",
+    primary: false,
+    description:
+      "Immerse yourself in a decentralized community where users actively contribute to Propspace's growth, creating a dynamic and inclusive space for real estate enthusiasts.",
+  },
+];
--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -1,8 +1,11 @@
 import heroImg from "../../assets/images/hero.svg";
 import houseImg from "../../assets/images/house.jpg";
 import downIcon from "../../assets/icons/down.svg";
+import grid from "../../assets/images/grid.png";
 import { Button } from "../../components/global/button";
 import { TextHighlighter } from "../../components/global/textHighlighter";
+import { FeaturesData } from "../../data/feature";
+
 export const Landing = () => {
   return (
     <>
@@ -59,7 +62,7 @@ export const Landing = () => {
           ></div>
           <p className="text-xl lg:w-1/2">
             <span className="text-mustard text-6xl">*</span>
-            Discover the{" "}
+            Discover the
             <TextHighlighter>
               future of real estate on Propspace
             </TextHighlighter>
@@ -74,6 +77,36 @@ export const Landing = () => {
               <Button text="Get Started" />
             </span>
           </p>
+        </div>
+      </section>
+
+      <section
+        className="mt-[200px] h-fit w-full space-y-10 px-20 pt-[0px]"
+        style={{
+          backgroundImage: `url(${grid})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <h2 className="place-self-center text-center text-7xl font-medium">
+          The Power <br /> of Propspace
+        </h2>
+        <div className="grid grid-cols-2 gap-10">
+          {FeaturesData.map((feature) => (
+            <div
+              className={`${feature.primary ? "bg-mustard" : "bg-white"} flex items-center gap-10 rounded-[25px] p-10 text-black`}
+              key={feature.id}
+            >
+              <span className="h-fit w-fit rounded-full bg-black p-4 text-4xl font-bold text-white">
+                {feature.id}
+              </span>
+              <div className="space-y-2">
+                <h2 className="text-4xl font-bold">{feature.title}</h2>
+                <p className="pr-12">{feature.description}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </section>
     </>
