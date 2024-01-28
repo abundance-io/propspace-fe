8fdb14d64aed8fdee27b6bfc11b75b02229d69c0
tanthehack <31630356+tanthehack@users.noreply.github.com>
2024-01-28 20:39:23 +0100

    add: blog styling and dummy blog data

--- src/pages/blog/blog.jsx
+++ src/pages/blog/blog.jsx
@@ -1,13 +1,34 @@
 import { Link } from "react-router-dom";
+import { BlogPosts } from "../../data/blogs";
+import grid from "../../assets/images/grid.png";
 
 export const AllBlogs = () => {
   return (
-    <section className="pt-[64px]">
-      <h1>all blogies</h1>
-      <ul>
-        <li>
-          <Link to="intro">first blog post</Link>
-        </li>
+    <section
+      className="flex h-screen flex-col items-center space-y-10 pt-[200px]"
+      style={{
+        backgroundImage: `url(${grid})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <h1 className="text-9xl font-medium">All Posts</h1>
+      <ul className="px-48">
+        {BlogPosts.map((post) => (
+          <li
+            key={post.id}
+            className="flex h-fit flex-col gap-4 rounded-[40px] border-[1px] border-white bg-black p-10"
+          >
+            <h1 className="text-mustard text-2xl uppercase">
+              <Link to="intro">{post.title}</Link>
+            </h1>
+            <p>{post.description}</p>
+            <p className="text-thin">
+              {post.date} - {post.author}
+            </p>
+          </li>
+        ))}
       </ul>
     </section>
   );
--- src/pages/blog/entry.jsx
+++ src/pages/blog/entry.jsx
@@ -1,3 +1,22 @@
+import { BlogPosts } from "../../data/blogs";
+import Markdown from "react-markdown";
+
 export const BlogEntry = () => {
-  return <h1>blog</h1>;
+  return (
+    <section className=" p-20">
+      {BlogPosts.map((post) => (
+        <div key={post.id} className="flex">
+          <div className="flex flex-col gap-8">
+            <h1 className="text-9xl">{post.title}</h1>
+            <p className="text-mustard">
+              {post.date} - {post.author}
+            </p>
+          </div>
+          <p className="text-xl">
+            <Markdown>{post.content}</Markdown>
+          </p>
+        </div>
+      ))}
+    </section>
+  );
 };
