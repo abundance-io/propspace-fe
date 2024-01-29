3a551a33de73ea143eeb49d668086c325356a7bf
tanthehack <31630356+tanthehack@users.noreply.github.com>
2024-01-29 12:34:09 +0100

    add: error page styling

--- src/pages/error/error.jsx
+++ src/pages/error/error.jsx
@@ -1,7 +1,27 @@
-import { useRouteError } from "react-router-dom";
+import { useNavigate, useRouteError } from "react-router-dom";
 
 export const Error = () => {
   const error = useRouteError();
+  const navigate = useNavigate();
 
-  return <h1>{error.statusText}</h1>;
+  return (
+    <main className="flex h-screen flex-col items-center justify-center gap-20">
+      <section className="flex flex-row items-center justify-center gap-10 pt-[0px]">
+        <h1 className="text-9xl font-bold">{error.status} </h1>
+        <span className="text-mustard text-9xl ">|</span>
+        <p className="text-2xl">
+          {error.statusText} {error.data}
+        </p>
+      </section>
+
+      <section className="pt-[0px]">
+        <button
+          onClick={() => navigate(-1)}
+          className="hover:text-mustard text-xl hover:underline"
+        >
+          Go Back
+        </button>
+      </section>
+    </main>
+  );
 };
