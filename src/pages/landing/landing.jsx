9a5671c38f12275fc2a1206155ffad79a2fb87f0
tanthehack <31630356+tanthehack@users.noreply.github.com>
2024-01-29 05:29:45 +0100

    fix: landing page responsiveness

--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -13,40 +13,43 @@ export const Landing = () => {
         id="home"
         className="font-body flex h-screen w-full flex-col items-center pt-[64px] lg:flex-row"
       >
-        <div className="space-y-10 px-24">
-          <h1 className="text-9xl font-medium">
+        <div className="space-y-10 px-10 lg:px-24">
+          <h1 className="text-6xl font-medium md:text-8xl lg:text-9xl">
             Secure <br /> Lifelong <br /> Earnings
           </h1>
-          <p className="w-[70%]">
+          <p className="lg:w-[70%]">
             A groundbreaking Real Estate Investment Platform built on the
             <TextHighlighter> Internet Computer Protocol (ICP)</TextHighlighter>
             , offering <TextHighlighter>revolutionary </TextHighlighter>
             opportunities for investors in the dynamic world of real estate.
           </p>
 
-          <div className="flex items-center gap-6">
+          <div className="flex flex-col gap-6 lg:flex-row lg:items-center">
             <Button text="Get Started" />
             <a href="#features" className="hover:underline">
               Our Features ↘
             </a>
           </div>
         </div>
-        <img src={heroImg} className="h-full animate-pulse" />
+        <img
+          src={heroImg}
+          className="absolute right-0 h-[50%] animate-pulse lg:relative lg:h-full"
+        />
       </section>
 
       <a
-        className="item-center flex w-full animate-bounce justify-center"
+        className="item-center hidden w-full animate-bounce justify-center lg:flex"
         href="#about"
       >
         <img src={downIcon} />
       </a>
 
-      <section className="flex flex-col gap-10 p-20" id="about">
+      <section className="flex flex-col gap-10 p-10 lg:p-20" id="about">
         <div className="flex flex-col items-center space-y-5">
           <span className="bg-mustard rounded-[12px] px-5 py-3 text-sm font-bold text-black">
             what we do
           </span>
-          <h2 className="text-center text-6xl font-medium">
+          <h2 className="text-center text-4xl font-medium lg:text-6xl">
             Revolutionizing Real Estate <br /> Investment for Africans
           </h2>
         </div>
@@ -60,7 +63,7 @@ export const Landing = () => {
             }}
             className="h-[200px] w-full rounded-[40px] lg:h-[500px] lg:w-1/2"
           ></div>
-          <p className="text-xl lg:w-1/2">
+          <p className="text-base lg:w-1/2 lg:text-xl">
             <span className="text-mustard text-6xl">*</span>
             Discover the
             <TextHighlighter>
@@ -69,7 +72,7 @@ export const Landing = () => {
             , where blockchain turns properties into tradable NFTs. Co-own
             properties, share profits, and participate in transparent governance
             through our ICP blockchain DAO.
-            <span className="flex flex-col gap-4 pt-[100px] lg:flex-row lg:items-center">
+            <span className="flex flex-col gap-4 pt-[50px] lg:flex-row lg:items-center lg:pt-[100px]">
               <p className="lg:w-3/5">
                 Join us in reshaping real estate investment for a more inclusive
                 and prosperous future.
@@ -82,7 +85,7 @@ export const Landing = () => {
 
       <section
         id="features"
-        className="mt-[200px] h-fit w-full space-y-20 px-20 pt-[0px]"
+        className="mt-[200px] h-fit w-full space-y-20 px-10 pt-[0px] lg:px-20"
         style={{
           backgroundImage: `url(${grid})`,
           backgroundSize: "cover",
@@ -90,21 +93,23 @@ export const Landing = () => {
           backgroundRepeat: "no-repeat",
         }}
       >
-        <h2 className="place-self-center text-center text-7xl font-medium">
+        <h2 className="place-self-center text-center text-4xl font-medium lg:text-7xl">
           The Power <br /> of Propspace
         </h2>
-        <div className="grid grid-cols-2 gap-10">
+        <div className="grid grid-cols-1 gap-10 lg:grid-cols-2">
           {FeaturesData.map((feature) => (
             <div
-              className={`${feature.primary ? "bg-mustard" : "bg-white"} flex items-center gap-10 rounded-[25px] p-10 text-black`}
+              className={`${feature.primary ? "bg-mustard" : "bg-white"} flex flex-col items-center gap-10 rounded-[25px] p-6 text-black lg:flex-row lg:p-10`}
               key={feature.id}
             >
-              <span className="h-fit w-fit rounded-full bg-black p-4 text-4xl font-bold text-white">
+              <span className="h-fit w-fit rounded-full bg-black p-4 text-2xl font-bold text-white lg:text-4xl">
                 {feature.id}
               </span>
               <div className="space-y-2">
-                <h2 className="text-4xl font-bold">{feature.title}</h2>
-                <p className="pr-12">{feature.description}</p>
+                <h2 className="text-2xl font-bold lg:text-4xl">
+                  {feature.title}
+                </h2>
+                <p className="lg:pr-12">{feature.description}</p>
               </div>
             </div>
           ))}
